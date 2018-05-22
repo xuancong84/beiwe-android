e092597b78042d0d54885c9cb778410daeb3c01e
Sean Esterkin <sean@zagaran.com>
2018-05-22 16:42:24 -0400

    Catch errors when creating new files to guard against headerless files

--- app/src/main/java/org/beiwe/app/networking/PostRequest.java
+++ app/src/main/java/org/beiwe/app/networking/PostRequest.java
@@ -197,7 +197,7 @@ public class PostRequest {
 			return 2; }
 		// Log.d( "PostRequest", "Received a key: " + key );
 		TextFileManager.getKeyFile().deleteSafely();
-		TextFileManager.getKeyFile().writePlaintext( key );
+		TextFileManager.getKeyFile().safeWritePlaintext( key );
 		return httpResponse;
 	}
 	
